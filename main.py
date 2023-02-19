6f707b39a74d1cdacfe12f125d60569d8680557c
orcunenes <vvalentos.1@gmail.com>
2023-02-19 22:17:04 +0300

    Progress

---
+++ main.py
@@ -0,0 +1,40 @@
+import pyautogui 
+from threading import Thread
+from pynput.mouse import Controller, Button
+from time import sleep
+from pynput.keyboard import KeyCode,Listener
+import tkinter as tk  
+
+delay=0.5
+mouse=Controller()
+
+res = pyautogui.locateOnScreen("image.png")
+print(res)
+pyautogui.moveTo(res)
+
+class AutoClicker(Thread):
+    clicking = False
+
+    def run(self):
+        try: 
+            while True:
+                    if AutoClicker.clicking:
+                        res = pyautogui.locateOnScreen("image.png",confidence=0.8)
+                        print(res)
+                        if res is not None:
+                            break
+                        mouse.click(Button.left)
+
+                    sleep(delay)
+        except KeyboardInterrupt:
+            pass
+
+def keypress(key):
+    if key == KeyCode(char="k"):
+        AutoClicker.clicking=not AutoClicker.clicking
+
+
+AutoClicker().start()
+
+with Listener(on_press=keypress) as listener:
+    listener.join()
